77b78942e4d19f4df735b5b9a62f6d4aa430f539
Felipe Melo Meireles do Nascimento <flpnascto@gmail.com>
2022-11-16 15:38:55 -0300

    define metodos privados em PlantValidate

--- src/services/validations/PlantValidate.ts
+++ src/services/validations/PlantValidate.ts
@@ -2,31 +2,31 @@ import { INewPlant } from '../../interfaces';
 import { BadRequestException } from '../../exceptions';
 
 export default class PlantValidate {
-  static validateBreed(breed: string): void {
+  private static validateBreed(breed: string): void {
     if (typeof breed !== 'string') {
       throw new BadRequestException('Attribute "breed" must be string.');
     }
   }
 
-  static validateNeedsSun(needsSun: boolean): void {
+  private static validateNeedsSun(needsSun: boolean): void {
     if (typeof needsSun !== 'boolean') {
       throw new BadRequestException('Attribute "needsSun" must be boolen.');
     }
   }
 
-  static validateOrigin(origin: string): void {
+  private static validateOrigin(origin: string): void {
     if (typeof origin !== 'string') {
       throw new BadRequestException('Attribute "origin" must be string.');
     }
   }
 
-  static validateSize(size: number): void {
+  private static validateSize(size: number): void {
     if (typeof size !== 'number') {
       throw new BadRequestException('Attribute "size" must be number.');
     }
   }
 
-  static validateAttibutes(plant: INewPlant): void {
+  public static validateAttibutes(plant: INewPlant): void {
     PlantValidate.validateBreed(plant.breed);
     PlantValidate.validateNeedsSun(plant.needsSun);
     PlantValidate.validateOrigin(plant.origin);
